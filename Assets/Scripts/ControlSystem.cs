d66edd0a9dc6eacbcd91341c14cb7113d2910cde
KID <50666207+KID421@users.noreply.github.com>
2023-05-07 12:13:42 +0800

    Update 完成控制系統

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -31,11 +31,33 @@ public class ControlSystem : MonoBehaviour
 
     private void Update()
     {
+        MoveAndAnimation();
+    }
+
+    private void MoveAndAnimation()
+    {
         // print("<color=yellow>這是更新事件~</color>");
 
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         rig.velocity = new Vector3(h, v, 0) * moveSpeed;
+
+        // 玩家有按 A 輸出 true，沒有輸出 false
+        // print(Input.GetKeyDown(KeyCode.A));
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        // 如果 h > 0 就開啟跑步動畫
+        // h > 0 或者 h < 0 或者 v > 0 或者 v < 0
+        ani.SetBool(parRun, h > 0 || h < 0 || v > 0 || v < 0);
     }
 }
